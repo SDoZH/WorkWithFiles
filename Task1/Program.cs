2f4a57d31ffbb7a1b9600d8c85e889330248a707
SDozH <soldatov_alex@bk.ru>
2022-07-18 02:03:39 +0300

    пром

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.IO;
+using System.IO.Enumeration;
+
 
 namespace Task1
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            DelCatalogs30();
+
+        }
+
+        static void DelCatalogs30()
+        {
+            var DelFailDir = new DirectoryInfo("C:/Users/Dzh/Desktop/Папка для 8 модуля");
+            string dirName = "C:/Users/Dzh/Desktop/Папка для 8 модуля";
+            if (DelFailDir.Exists)
+            {
+                Console.WriteLine("Папка существует");
+                Console.WriteLine("Папки:");
+                DateTime OpenDateDir = Directory.GetLastAccessTime(dirName); //Получим время открытия папок
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории в папке
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine("{0} последнее использование {1}", d, OpenDateDir);
+
+                Console.WriteLine("Файлы:");
+
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы в папке
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+
+
+                foreach (FileInfo file in DelFailDir.GetFiles())
+                {
+                    try
+                    {
+                        var timeSpan = file.LastAccessTime;
+                        Console.WriteLine(timeSpan.ToString());
+                        timeSpan.Add(TimeSpan.FromMinutes(30));
+                        file.Delete();
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Нет подходящих для удаления файлов ");
+                    }
+
+                }
+                foreach (DirectoryInfo dir in DelFailDir.GetDirectories())
+                {
+                    var timeSpan = dir.LastAccessTime;
+                    Console.WriteLine(timeSpan.ToString());
+                    var timeSpan1 = DateTime.Now;
+                    var timeSpan2 = TimeSpan.FromMinutes(30);
+                    var duras = timeSpan1 - timeSpan;
+                    dir.Delete(true);
+
+                }
+
+            }
         }
     }
 }
+
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.IO.Enumeration;
 
 namespace Task4
 {
