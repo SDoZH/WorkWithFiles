11dc8cee0bd6487d71630b2e7aa9b9dc6c42755f
SDozH <soldatov_alex@bk.ru>
2022-07-18 02:35:04 +0300

    Task2 готов

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,11 +4,55 @@ using System.IO.Enumeration;
 
 namespace Task2
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pathToDirectory = "C:/Users/Dzh/Desktop/Папка для 8 модуля";
+            double catalogSize = 0;
+            catalogSize = sizeFolder(pathToDirectory, ref catalogSize); 
+            if (catalogSize != 0)
+            {
+                Console.WriteLine("Размер {0} \nсоставляет {1} байт ", pathToDirectory, catalogSize);
+            }
+            else
+            {
+                Console.WriteLine("Каталог {0} пуст.", pathToDirectory);
+            }
+            Console.ReadLine();
+        }
+
+        static double sizeFolder(string folder, ref double catalogSize)
+        {
+            try
+            {
+                DirectoryInfo Fol = new DirectoryInfo(folder);
+                DirectoryInfo[] dir = Fol.GetDirectories();
+                FileInfo[] fi = Fol.GetFiles();
+                foreach (FileInfo f in fi)
+                {
+                    catalogSize = catalogSize + f.Length;
+                }
+                foreach (DirectoryInfo df in dir)
+                {
+                    sizeFolder(df.FullName, ref catalogSize);
+                }
+                //1ГБ = 1024 Байта * 1024 КБайта * 1024 МБайта
+                return Math.Round((double) (catalogSize), 1);
+            }
+           
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Папка не существует. Ошибка: " + ex.Message);
+                return 0;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Нет доступа. Ошибка: " + ex.Message);
+                return 0;
+            }
+            
         }
     }
 }
