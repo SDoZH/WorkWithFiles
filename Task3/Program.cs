41c7df8eaf500dcb1f79a4a0c9966c65a51ae595
SDozH <soldatov_alex@bk.ru>
2022-07-19 13:41:26 +0300

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -8,91 +8,122 @@ namespace Task3
     {
         public static void Main()
         {
-            DelCatalogs30();
-        }
-        static void DelCatalogs30()
-        {
-            var DelFailDir = new DirectoryInfo("C:/Users/Dzh/Desktop/Папка для 8 модуля");
             string pathToDirectory = "C:/Users/Dzh/Desktop/Папка для 8 модуля";
             double catalogSize = 0;
-            catalogSize = sizeFolder(pathToDirectory, ref catalogSize);
-            if (DelFailDir.Exists)
+            double catalogSize2 = 0;
+            catalogSize = SizeFolder(pathToDirectory, ref catalogSize);
+            Console.WriteLine("Исходный размер {0} составляет {1} байт ", pathToDirectory, catalogSize);
+            DelCatalogs30();
+            SizeFolder2(pathToDirectory, ref catalogSize2);
+            Console.WriteLine("Удалено {0} байт ",catalogSize - catalogSize2);
+            Console.WriteLine("Текущий размер {0} составляет {1} байт ", pathToDirectory, catalogSize2);
+           
+
+            static void DelCatalogs30()
             {
-                if (catalogSize != 0)
+                var DelFailDir = new DirectoryInfo("C:/Users/Dzh/Desktop/Папка для 8 модуля");
+                if (DelFailDir.Exists)
                 {
-                    Console.WriteLine("Исходный размер каталога {0} -- {1} байт ", pathToDirectory, catalogSize);
-                }
-  
-                foreach (FileInfo file in DelFailDir.GetFiles())
-                {
-                    var timeSpan = file.LastAccessTime;
-                    var timeSpan1 = DateTime.Now;
-                    var timeSpan2 = TimeSpan.FromMinutes(30);
-                    var duras = timeSpan1 - timeSpan;
-                    if (duras >= timeSpan2)
-                    { file.Delete(); }
-                    else
+                    foreach (FileInfo file in DelFailDir.GetFiles())
                     {
-                        Console.WriteLine();
-                        Console.WriteLine("Нет файлов,которые не используются более 30 мин.");
-                        Console.WriteLine($"Прошло {duras} времени");
-                        break;
+                        var timeSpan = file.LastAccessTime;
+                        var timeSpan1 = DateTime.Now;
+                        var timeSpan2 = TimeSpan.FromMinutes(2);
+                        var duras = timeSpan1 - timeSpan;
+                        if (duras >= timeSpan2)
+                        { file.Delete(); }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Нет файлов,которые не используются более 30 мин.");
+                            Console.WriteLine($"Прошло {duras} времени");
+                            break;
+                        }
+                    }
+                    foreach (DirectoryInfo dir in DelFailDir.GetDirectories())
+                    {
+                        var timeSpan = dir.LastAccessTime;
+                        var timeSpan1 = DateTime.Now;
+                        var timeSpan2 = TimeSpan.FromMinutes(2);
+                        var duras = timeSpan1 - timeSpan;
+                        if (duras >= timeSpan2)
+                        {
+                            dir.Delete(true);
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Нет папок,которыене используются более 30 мин");
+                            Console.WriteLine($"Прошло {duras} времени");
+                            break;
+                        }
                     }
                 }
-
-                foreach (DirectoryInfo dir in DelFailDir.GetDirectories())
-                {
-                    var timeSpan = dir.LastAccessTime;
-                    var timeSpan1 = DateTime.Now;
-                    var timeSpan2 = TimeSpan.FromMinutes(30);
-                    var duras = timeSpan1 - timeSpan;
-                    if (duras >= timeSpan2)
-                    {
-                        dir.Delete(true);
-                    }
-                    else
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Нет папок,которыене используются более 30 мин");
-                        Console.WriteLine($"Прошло {duras} времени");
-                        break;
-                    }
-                }
-
             }
+            static double SizeFolder(string pathToDirectory, ref double catalogSize) //метод для подсчета
+            {
+                try
+                {
+                    DirectoryInfo Fol = new DirectoryInfo(pathToDirectory);
+                    DirectoryInfo[] dir = Fol.GetDirectories();
+                    FileInfo[] fi = Fol.GetFiles();
+                    foreach (FileInfo f in fi)
+                    {
+                        catalogSize = catalogSize + f.Length; //складываем
+                    }
+                    foreach (DirectoryInfo df in dir)
+                    {
+                        SizeFolder(df.FullName,ref catalogSize); // рекурсивно вызываем метод
+                    }
+                    return Math.Round((double)(catalogSize), 1);
+                }
 
+                catch (DirectoryNotFoundException ex)
+                {
+                    Console.WriteLine("Папка не существует. Ошибка: " + ex.Message);
+                    return 0;
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine("Нет доступа. Ошибка: " + ex.Message);
+                    return 0;
+                }
+            }
+            static double SizeFolder2(string pathToDirectory,ref double catalogSize2) //метод для подсчета
+            {
+                try
+                {
+                    DirectoryInfo Fol = new DirectoryInfo(pathToDirectory);
+                    DirectoryInfo[] dir = Fol.GetDirectories();
+                    FileInfo[] fi = Fol.GetFiles();
+                    foreach (FileInfo f in fi)
+                    {
+                        catalogSize2 = catalogSize2 + f.Length; //складываем
+                    }
+                    foreach (DirectoryInfo df in dir)
+                    {
+                        SizeFolder2(df.FullName, ref catalogSize2); // рекурсивно вызываем метод
+                    }
+                    return Math.Round((double)(catalogSize2), 1);
+                }
+
+                catch (DirectoryNotFoundException ex)
+                {
+                    Console.WriteLine("Папка не существует. Ошибка: " + ex.Message);
+                    return 0;
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    Console.WriteLine("Нет доступа. Ошибка: " + ex.Message);
+                    return 0;
+                }
+            }
         }
-        static double sizeFolder(string folder, ref double catalogSize) //метод для подсчета
-        {
-            try
-            {
-                DirectoryInfo Fol = new DirectoryInfo(folder);
-                DirectoryInfo[] dir = Fol.GetDirectories();
-                FileInfo[] fi = Fol.GetFiles();
-                foreach (FileInfo f in fi)
-                {
-                    catalogSize = catalogSize + f.Length; //складываем
-                }
-                foreach (DirectoryInfo df in dir)
-                {
-                    sizeFolder(df.FullName, ref catalogSize); // рекурсивно вызываем метод
-                }
-                return Math.Round((double)(catalogSize), 1);
-            }
 
-            catch (DirectoryNotFoundException ex)
-            {
-                Console.WriteLine("Папка не существует. Ошибка: " + ex.Message);
-                return 0;
-            }
-            catch (UnauthorizedAccessException ex)
-            {
-                Console.WriteLine("Нет доступа. Ошибка: " + ex.Message);
-                return 0;
-            }
-
-        }
     }
 }
+
+
+
 
 
